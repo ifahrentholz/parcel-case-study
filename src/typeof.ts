0eb6738e88a44e804d40d8248742316afb96166e
Ingo Fahrentholz <ingof.work@gmail.com>
2019-01-27 23:24:44 +0100

    Typeguard "typeof"

---
+++ src/typeof.ts
@@ -0,0 +1,27 @@
+class Song {
+  constructor(public title: string, public duration: string | number) {}
+}
+
+function getSongDuration(item: Song) {
+  if (typeof item.duration === "string") {
+    return item.duration;
+  }
+
+  const { duration } = item;
+  const minutes = Math.floor(duration / 60000);
+  const seconds = (duration / 1000) % 60;
+
+  return `${minutes}:${seconds}`;
+}
+
+const songDurationFromString = getSongDuration(
+  new Song("Wonderful Wonderful", "5:31")
+);
+
+console.log(songDurationFromString);
+
+const getSongDurationFromMs = getSongDuration(
+  new Song("Wonderful Wonderful", 330000)
+);
+
+console.log(getSongDurationFromMs);
