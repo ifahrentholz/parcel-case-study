0667a4a6a6d02c91aef8ddac77f6919d4e38ddfa
Ingo Fahrentholz <ingof.work@gmail.com>
2019-01-27 20:09:16 +0100

    Add some Typescript exercises

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,36 @@
-class Greeter {
-  constructor(public name: string) {}
+const person = {
+  name: "Ingo Person",
+  age: 33,
+  surname: "Fahrentholz"
+};
 
-  say() {
-    console.log(`Hi ${this.name}!`);
-    return "hi";
-  }
+interface Book {
+  author: string;
+  pages?: number;
 }
 
-const greeter = new Greeter("Ingo");
-greeter.say();
+const book: Book = { author: "Ingo Author", pages: 300 };
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+
+const personName = getProperty(person, "name");
+const authorName = getProperty(book, "author");
+const bookPages = getProperty(book, "pages");
+
+console.log("personName", personName);
+console.log("authorName", authorName);
+console.log("bookPages", bookPages);
+
+// READONLY
+type MyReadonly<T> = { readonly [P in keyof T]: T[P] };
+
+function freeze<T>(obj: T): MyReadonly<T> {
+  return Object.freeze(obj);
+}
+
+const newPerson = freeze(book);
+
+// As expected this would'nt work.
+// newPerson.author = "YUSUF"
